995508eb3ec225313ba83dbb68b66cfbf65a0d0a
Siddhant Kulkarni <kulkarni.sid0809@gmail.com>
2024-08-09 04:57:59 +0530

    Initialize subgraph

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,17 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-toolbox"); //new update from hardhat requires to import 'hardhat-toolbox'
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.24",
+  solidity: "0.8.4",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    amoy: {
+      url: process.env.STAGING_INFURA_URL,
+      accounts: [`0x${process.env.STAGING_PRIVATE_KEY}`],
+      gas: 2100000,
+      gasPrice: 25000000000,
+    },
+  },
 };
